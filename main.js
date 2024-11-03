aca4868a7c3d752797dc255232224810b824c1c9
DanskuVV <danielvv@metropolia.fi>
2024-11-04 00:40:32 +0200

    GLTF, environment, blender models

--- main.js
+++ main.js
@@ -1,74 +1,81 @@
 import * as THREE from "three";
+
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+let camera, scene, renderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true; // Enable shadow maps
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+init();
 
-const controls = new OrbitControls(camera, renderer.domElement);
+function init() {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
 
-// Add a light source
-const light = new THREE.PointLight(0xffffff, 200, 100);
-light.position.set(0, 10, 0);
-light.castShadow = true; // Enable shadows for the light
-scene.add(light);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.25,
+    20
+  );
+  camera.position.set(-1.8, 0.6, 2.7);
 
-// add a point light to the side
-const light2 = new THREE.PointLight(0xffffff, 100, 100);
-light2.position.set(0, 0, 10);
-light2.castShadow = true; // Enable shadows for the light
-scene.add(light2);
+  scene = new THREE.Scene();
 
-// Add ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+  new RGBELoader()
+    .setPath("images/")
+    .load("qwantani_dusk_2_4k.hdr", function (texture) {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
 
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set(0, 20, 100);
-controls.update();
+      scene.background = texture;
+      scene.environment = texture;
 
-// Create the floor
-const floorGeometry = new THREE.PlaneGeometry(10, 10);
-const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xfffff });
-const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.rotation.x = -Math.PI / 2; // Rotate the floor to be horizontal
-floor.position.y = -2; // Position the floor under the cube
-floor.receiveShadow = true; // Enable shadow receiving for the floor
-scene.add(floor);
+      render();
 
-// Create a wall perpendicular to the floor
-const wallGeometry = new THREE.PlaneGeometry(10, 10);
-const wallMaterial = new THREE.MeshStandardMaterial({ color: 0xfffff });
-const wall = new THREE.Mesh(wallGeometry, wallMaterial);
-wall.position.z = -5; // Position the wall behind the cube
-wall.receiveShadow = true; // Enable shadow receiving for the wall
-scene.add(wall);
+      // model
 
-// Create a cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x5a47c9 });
-const cube = new THREE.Mesh(geometry, material);
-cube.castShadow = true; // Enable shadow casting for the cube
-scene.add(cube);
+      const loader = new GLTFLoader().setPath("Barrel/");
+      loader.load("Barrel2.gltf", async function (gltf) {
+        const model = gltf.scene;
 
-camera.position.z = 5;
+        // wait until the model can be added to the scene without blocking due to shader compilation
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+        await renderer.compileAsync(model, camera, scene);
 
-  // required if controls.enableDamping or controls.autoRotate are set to true
+        scene.add(model);
+
+        render();
+      });
+    });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render); // use if there is no animation loop
+  controls.minDistance = 2;
+  controls.maxDistance = 10;
+  controls.target.set(0, 0, -0.2);
   controls.update();
 
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
   renderer.render(scene, camera);
 }
